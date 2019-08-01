968432a16e4d88ddb8c0ae52f15c820a01c61400
gaoxupeng <252211829@qq.com>
2019-08-01 23:22:22 +0800

    图片上传相关代码

--- UniversalMall/mall_upload/src/main/java/com/mall/MallUploadApplication.java
+++ UniversalMall/mall_upload/src/main/java/com/mall/MallUploadApplication.java
@@ -2,8 +2,10 @@ package com.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MallUploadApplication {
 
     public static void main(String[] args) {
